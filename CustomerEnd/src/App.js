28bdd095f7783729443534ed2b8424009fee5ac4
SIDDESH THORAT <siddesh.thorat@rsllearning.com>
2021-05-20 13:34:32 +0530

    Removing unncessary files and restructuring folder

--- CustomerEnd/src/App.js
+++ CustomerEnd/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header/Header.component';
+import Analysis from './Component/Analysis/Analysis.component'
+import CommonAnnouncement from './Component/CommonAnnouncement/CommonAnnouncement.component'
+import ChatSection from './Component/ChatSection/ChatSection.component'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <div>
+            <Header />
+            <Analysis />
+            <CommonAnnouncement/>
+            <ChatSection/>
+        </div>
+    );
 }
-
 export default App;
