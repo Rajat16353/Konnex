77fe6a3db0c74ed5b291a037d3dedce4b2a7830b
shivanih99 <hebbaleshivani@gmail.com>
2021-05-21 12:04:02 +0530

    Creating components on customer end

--- CustomerEnd/src/Component/ChatSection/ChatSection.component.jsx
+++ CustomerEnd/src/Component/ChatSection/ChatSection.component.jsx
@@ -1,12 +1,22 @@
 import './ChatSection.style.css'
-
-
+import SendIcon from '@material-ui/icons/Send';
 const ChatSection = () => {
     return(
-    <div className="chatSection">
-        
-    </div>
-    )
-} 
-
+     <div class="chat-box">
+        <div class="chat-box-header">
+            ChatBot
+        </div>
+        <div class="chat-box-body">
+            <div class="chat-box-overlay"> </div>
+                <div class="chat-logs"> </div>
+            </div>
+            <div class="chat-input">      
+                <form>
+                    <input type="text" id="chat-input" placeholder="Send a message..."/>
+                    <button type="submit" class="chat-submit" id="chat-submit"><SendIcon/></button>
+                </form>      
+            </div>
+        </div>
+    );
+}
 export default ChatSection;
--- CustomerEnd/src/Component/Header/Header.component.jsx
+++ CustomerEnd/src/Component/Header/Header.component.jsx
@@ -1,15 +1,9 @@
 import './Header.style.css'
 const Header = () => {
     return(
-          <div class="title">
-              <h1>KONEX</h1>
-          </div>
-
-
-
-
-
-        )
-    } 
-    
-    export default Header;
+        <div class="title">
+            <h1>KONEX</h1>
+        </div>
+    )
+} 
+export default Header;
